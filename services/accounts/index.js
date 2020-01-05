fe94a0a82b6e7a929ab5ee61fce59cd9b0971f75
colvint <taurus.colvin@gmail.com>
2020-01-05 14:53:27 -0500

    fix the accounts service

--- services/accounts/index.js
+++ services/accounts/index.js
@@ -2,21 +2,20 @@ const app = require('express')();
 const accountsService = require('http').createServer(app);
 const accountsSocketServer = require('socket.io')(accountsService);
 
-const { authenticateConnection, logClientConnectedEvent, logSocketListeningEvent } = require('../../packages/connections');
-const { JWT, SERVICES: { ACCOUNTS } } = require('../../config');
+const { CONNECTIONS: { authenticateConnection, logClientConnectedEvent, logSocketListeningEvent }, STEPS: { ackStep } } = require('@aqueoss/system');
+const { JWT, SERVICES: { ACCOUNTS } } = require('@your-organization/config');
 // const { validateAction } = require('../../system/lib/action-handling');
-const STEP_TYPES = require('../../system/actions/steps/types');
-const EVENT_TYPES = require('../../packages/events/types');
+const { EVENTS, STEPS: { createAccounts, STEP_TYPES } } = require('@your-organization/system');
 
 const setupHandlers = socket =>
   socket
     // .use(validateAction({ socket, VALIDATORS }))
-    .on(STEP_TYPES.CREATE_ACCOUNTS, callStep(createAccounts({ socket })));
+    .on(STEP_TYPES.CREATE_ACCOUNTS, ackStep(createAccounts({ socket })));
 
 accountsSocketServer
   .use(authenticateConnection(JWT.SERVER_SECRET))
-  .on(EVENT_TYPES.SOCKET_CONNECTED, setupHandlers)
-  .on(EVENT_TYPES.SOCKET_CONNECTED, logClientConnectedEvent);
+  .on(EVENTS.SOCKET_CONNECTED, setupHandlers)
+  .on(EVENTS.SOCKET_CONNECTED, logClientConnectedEvent);
 
 accountsService
   .listen(ACCOUNTS.SOCKET_PORT, () => logSocketListeningEvent(ACCOUNTS));
